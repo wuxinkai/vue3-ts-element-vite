5a2bdf4e1cc0b566089609fb0222c52a68492d8b
wuxinkai <275648208@qq.com>
2022-09-02 16:48:14 +0800

    first commit

--- src/components/menu/infiniteMenu.tsx
+++ src/components/menu/infiniteMenu.tsx
@@ -1,56 +1,74 @@
-import { toLine } from "@/utils";
 import { defineComponent, PropType, useAttrs } from "vue";
-import { MenuItem } from "./types";
 import * as Icons from '@element-plus/icons-vue'
 export default defineComponent({
     props: {
         //
         data: {
-            type: Array as PropType<MenuItem[]>,
+            type: Array as PropType<any[]>,
             require: true
         },
         // 默认选中菜单
         defaultActive: {
             type: String,
-            default: "", 
+            default: "",
         },
         // 是否是路由模式
         router: {
             type: Boolean,
             default: false,
-        }
+        },
+        //设置键名  菜单表头签名
+        name: {
+            type: String,
+            default: 'name',
+        },
+        //设置键名  路由签名
+        index: {
+            type: String,
+            default: 'index',
+        },
+        //设置键名  图标签名
+        icon: {
+            type: String,
+            default: 'icon',
+        },
+        //设置键名 子数组的签名
+        children: {
+            type: String,
+            default: 'children',
+        },
     },
-    
+
     setup(props, ctx) {
         //封装一个无限极的菜单
         //函数返回一段jsx代码
-        let renderMenu = (data: MenuItem[]) => {
-            return data.map((item: MenuItem) => {
+        let renderMenu = (data: any[]) => {
+            return data.map((item: any) => {
                 //每个菜单的一个图标
 
-                item.i = (Icons as any)[item.icon!]
+                item.i = (Icons as any)[item[props.icon!]]
                 //处理插槽
                 let slots = {
                     title: () => {
                         return (
                             <>
                                 <> {item.i ? <item.i /> : ''}</>
-                                <span>{item.name}</span>
+                                <span>{item[props.name]}</span>
                             </>)
                     }
                 }
                 //递归渲染子菜单
-                if (item.children && item.children.length > 0) {
+                if (item[props.children!] && item[props.children!].length > 0) {
                     return (
-                        <el-sub-menu index={item.index} v-slots={slots}>
-                            {renderMenu(item.children)}
+                        <el-sub-menu index={item[props.index]} v-slots={slots}>
+                            {renderMenu(item[props.children!])}
                         </el-sub-menu>
                     )
                 }
                 //没有子菜单显示状态
-                return (<el-menu-item index={item.index}>
+                return (<el-menu-item index={item[props.index]}>
                     <> {item.i ? <item.i /> : ''}</>
-                    <span>{item.name}</span>
+                    <span>{item[props.name]}</span>
                 </el-menu-item>)
             })
         }
--- src/views/menu/menu.vue
+++ src/views/menu/menu.vue
@@ -1,90 +1,88 @@
 <template>
-    <div class="container">
-      <div style="width: 200px">
-        <pro-menu :data="data1" defaultActive="2" text-color="green" active-text-color="red" background-color="#ccc"></pro-menu>
-      </div>
-      <div style="width: 200px">
-        <pro-infinite-menu
-          :data="data2"
-          defaultActive="1"
-          text-color="green"
-          background-color="pink"
-        ></pro-infinite-menu>
-      </div>
+  <div class="container">
+    <div style="width: 200px">
+      <pro-menu :data="data1" defaultActive="2" text-color="green" active-text-color="red" background-color="#ccc">
+      </pro-menu>
     </div>
-  </template>
+    <div style="width: 200px">
+      <!-- 自定义键名 -->
+      <pro-infinite-menu :data="data2" defaultActive="1" text-color="green" background-color="pink" name="a" index="b"
+        icon="c" children="xxxx"></pro-infinite-menu>
+    </div>
+  </div>
+</template>
   
   <script lang="ts" setup>
-  let data1 = [
-    {
-      name: "导航一",
-      index: "1",
-      icon: "Document",
-    },
-    {
-      name: "导航二",
-      index: "2",
-      icon: "Document",
-    },
-    {
-      name: "导航三",
-      index: "3",
-      icon: "Document",
-      children: [
-        {
-          name: "导航3-1",
-          index: "3-1",
-          icon: "Document",
-        },
-      ],
-    },
-  ];
-  let data2 = [
-    {
-      name: "导航一",
-      index: "1",
-      icon: "Document",
-    },
-    {
-      name: "导航二",
-      index: "2",
-      icon: "Document",
-    },
-    {
-      name: "导航三",
-      index: "3",
-      icon: "Document",
-      children: [
-        {
-          name: "导航3-1",
-          index: "3-1",
-          icon: "Document",
-          children: [
-            {
-              name: "导航3-1-1",
-              index: "3-1-1",
-              icon: "Document",
-              children: [
-                {
-                  name: "导航3-1-1-1",
-                  index: "3-1-1-1",
-                  icon: "Document",
-                  children: [
-                    {
-                      name: "导航3-1-1-1",
-                      index: "3-1-1-1",
-                      icon: "Document",
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ];
-  </script>
+let data1 = [
+  {
+    name: "导航一",
+    index: "1",
+    icon: "Document",
+  },
+  {
+    name: "导航二",
+    index: "2",
+    icon: "Document",
+  },
+  {
+    name: "导航三",
+    index: "3",
+    icon: "Document",
+    children: [
+      {
+        name: "导航3-1",
+        index: "3-1",
+        icon: "Document",
+      },
+    ],
+  },
+];
+let data2 = [
+  {
+    a: "导航一",
+    b: "1",
+    c: "Document",
+  },
+  {
+    a: "导航二",
+    b: "2",
+    c: "Document",
+  },
+  {
+    a: "导航三",
+    b: "3",
+    c: "Document",
+    xxxx: [
+      {
+        a: "导航3-1",
+        b: "3-1",
+        c: "Document",
+        xxxx: [
+          {
+            a: "导航3-1-1",
+            b: "3-1-1",
+            c: "Document",
+            xxxx: [
+              {
+                a: "导航3-1-1-1",
+                b: "3-1-1-1",
+                icon: "Document",
+                xxxx: [
+                  {
+                    a: "导航3-1-1-1",
+                    b: "3-1-1-1",
+                    c: "Document",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+</script>
   
   <style lang="scss" scoped>
   .container {
